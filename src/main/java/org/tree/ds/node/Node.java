e9131182791da68f8f786139e9cba7924036a89e
samoon-chaudhary <85609170+samoon-chaudhary@users.noreply.github.com>
2021-06-13 00:30:25 +0530

    Update Node.java

--- src/main/java/org/tree/ds/node/Node.java
+++ src/main/java/org/tree/ds/node/Node.java
@@ -5,11 +5,6 @@ public class Node {
     public Node right;
     public int data;
 
-
-    public Node(int data) {
-        this.data = data;
-    }
-
     public Node(){}
     public Node(int data) {
         this.data = data;
