4eb1e9fff2aa91fa616c4381f238d4bf9a60e4dc
canwassim <76614446+canwassim@users.noreply.github.com>
2021-06-20 19:12:00 +0530

    inOrderTraversal, preOrderTraversal, postOrderTraversal added

--- src/main/java/org/tree/ds/App.java
+++ src/main/java/org/tree/ds/App.java
@@ -4,24 +4,26 @@ import org.tree.ds.node.Node;
 
 import java.util.LinkedList;
 import java.util.Queue;
-
-/**
- * Hello world!
- *
- */
+/*Tree Traversals */
 public class App 
 {
     public static void main( String[] args )
     {
-
-
-
-       Node root=new Node().createNode();
-
+        Node root=new Node().createNode();
+        System.out.println("------------ all tree nodes----------------");
         showTree(root);
         System.out.println();
-        System.out.println("-------------------------");
+        System.out.println("------------ levelOrderTraversal------------");
         levelOrderTraversal(root);
+        System.out.println();
+        System.out.println("------------- InOrderTraversal--------------");
+        inOrderTraversal(root);
+        System.out.println();
+        System.out.println("------------- preOrderTraversal-------------");
+        preOrderTraversal(root);
+        System.out.println();
+        System.out.println("------------- postOrderTraversal------------");
+        postOrderTraversal(root);
     }
 
     private static void showTree(Node root) {
@@ -43,4 +45,23 @@ public class App
             if(element.right!=null) list.add(element.right);
         }
     }
+    private static void inOrderTraversal(Node root){
+        if(root==null) return;
+        inOrderTraversal(root.left);
+        System.out.print(root.data+" ");
+        inOrderTraversal(root.right);
+    }
+    private static void preOrderTraversal(Node root){
+        if(root==null) return;
+        System.out.print(root.data+" ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+    private static void postOrderTraversal(Node root) {
+        if(root==null) return;
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.print(root.data+" ");
+    }
+
 }
